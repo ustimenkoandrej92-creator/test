54d1e6514ed3351ba36fdab0942852208a3c5cec
ustimenkoandrej92-creator <ustimenkoandrej92@gmail.com>
2026-02-18 09:26:11 +0300

    Update programm.cpp

--- programm.cpp
+++ programm.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     int a, b;
-    cout << "Enter numbers" << endl;
+    cout << "Enter numbers a, b" << endl;
     cin >> a >> b;
     cout << "Sum = " << a + b << endl;
     cout << "Product = " << a + b << endl;
