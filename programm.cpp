783bc9eb1ba96f02e178a793dbd63fdd0485b208
ustimenkoandrej92-creator <ustimenkoandrej92@gmail.com>
2026-02-18 09:22:39 +0300

    Revert "Update programm.cpp"
    This reverts commit 567ac535061c4528a84317af5dfacd645164d8b1.

--- programm.cpp
+++ programm.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     int a, b;
-    cout << "Enter numbers a and b" << endl;
+    cout << "Enter numbers" << endl;
     cin >> a >> b;
     cout << "Sum = " << a + b << endl;
     cout << "Product = " << a + b << endl;
